5ce6f9fb4dd255d1b18ac2fa3c6ed92ee4200f7d
Sova <huntasubs@gmail.com>
2015-08-26 09:23:38 -0400

    .

---
+++ snip.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+encoder="/usr/local/bin/x264"
+prefix="~/Mocha/"
+videoname="video.m4v"
+
+#echo -n "Enter video path: "
+#read vid
+echo -n "Enter start frame: "
+read sframe
+echo -n "Enter total frames: "
+read eframe
+#declare -i n
+n=$eframe-$sframe
+
+#echo $prefix$videoname
+#$encoder
+#/usr/bin/x264 --crf 16 --tune fastdecode -i 250 --fps 23.976 --sar 1:1 --seek $startf --frames lenf -o $prefix$videoname
+$encoder --crf 16 --tune fastdecode -i 250 --fps 23.976 --sar 1:1 --seek $sframe --frames $n -o $prefix$videoname "$*"
+ffmpeg -i $prefix$videoname -f image2 $prefix/image%3d.png
